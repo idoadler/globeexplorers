76afb4912bb4d7e63d18aef9580c541fe83deaec
DESKTOP-3RI9R03\ido <games@ido.wtf>
2017-05-25 16:42:09 +0300

    CSV reading

---
+++ Assets/Scripts/CSV/ReadDemo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadDemo : MonoBehaviour
+{
+
+    public TextAsset csv;
+    public Text display;
+
+    void Start()
+    {
+        string[,] parsed = CSVReader.SplitCsvGrid(csv.text);
+        CSVReader.DebugOutputGrid(parsed);
+        string result = "";
+        for (int i = 0; i < parsed.GetLength(1); i++)
+        { 
+            for (int j = 0; j < parsed.GetLength(0); j++)
+            {
+                result += parsed[j, i] + " | ";
+            }
+            result += "\n";
+        }
+        display.text = result;
+    }
+}
