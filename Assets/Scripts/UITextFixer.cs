41d89c5769f3c40e0aa73c9875cf6165c0af8d33
Mickey Hoz <nimiauredhel@gmail.com>
2017-05-25 16:25:37 +0300

    Initial UI structure, imported assets & Hebrew text plugin

---
+++ Assets/Scripts/UITextFixer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using ArabicSupport;
+
+public class UITextFixer : MonoBehaviour
+{
+    public bool tashkeel = true;
+    public bool hinduNumbers = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        Text text = gameObject.GetComponent<Text>();
+        text.text = ArabicFixer.Fix(text.text, tashkeel, hinduNumbers);
+    }
+}
